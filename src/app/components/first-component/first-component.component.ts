83afa80d1a60073c7f8f84c137f2826cd378d942
Lorena Emmerick de Oliveira <lorenae@ciandt.com>
2024-02-09 12:17:04 -0300

    incluindo interpolação de dados e compartilhar dados entre pai e filho

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -9,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class FirstComponentComponent implements OnInit {
   name: string = "Lorena";
   age: number = 30;
+  job = "Programadora";
+  hobbies = ["Correr", "Jogar", "Estudar"];
+  car = {
+    name: "Polo",
+    year: 2000,
+  }
 
   constructor() { }
 
